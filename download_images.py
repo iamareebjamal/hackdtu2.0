48be1043ad1f3c0750c4d4418977e7a09bb6845e
RJ722 <rahul722j@gmail.com>
2017-10-14 08:44:18 +0530

    Add multiple urls for same disease

--- download_images.py
+++ download_images.py
@@ -4,24 +4,29 @@ import sys
 
 
 urls = {
-    'ringworm': 'http://www.dermnet.com/dn2/allJPG3/tinea-face',
-    'vitiligo': 'http://www.dermnet.com/dn2/allJPG3/vitiligo',
-    'warts': 'http://www.dermnet.com/dn2/allJPGThumb3/warts',
-    'eczema' : 'http://www.dermnet.com/dn2/allJPG3/eczema-leg'
+    'ringworm': ['http://www.dermnet.com/dn2/allJPG3/tinea-face','http://www.dermnet.com/dn2/allJPGThumb3/tinea-border','http://www.dermnet.com/dn2/allJPG3/tinea-scalp'],
+    'vitiligo': ['http://www.dermnet.com/dn2/allJPG3/vitiligo'],
+    'warts': ['http://www.dermnet.com/dn2/allJPGThumb3/warts','http://www.dermnet.com/dn2/allJPGThumb3/warts-common','http://www.dermnet.com/dn2/allJPGThumb3/warts-cryotherapy'],
+    'eczema' : ['http://www.dermnet.com/dn2/allJPG3/eczema-leg','http://www.dermnet.com/dn2/allJPGThumb3/eczema-hand','http://www.dermnet.com/dn2/allJPGThumb3/eczema-face']
 }
 
 
 def main(n):
     for disease in urls.keys():
         if not os.path.exists(disease):
-            os.makedirs(disease)
+                    os.makedirs(disease)
         for count in range(1, n+1):
-            prepend = '-%d.jpg' % (count)
-            q = requests.get(urls[disease] + prepend)
+            print (disease," : ",count)
+            for url in urls[disease]:
+                prepend = '-%d.jpg' % (count)
+                try:
+                    q = requests.get(url + prepend)
+                except exception as e:
+                    print e
 
-            filename = os.path.join(disease, str(count) + ".jpg")
-            with open(filename, "w") as f:
-                f.write(q.content)
+                filename = os.path.join(disease, str(count) + ".jpg")
+                with open(filename, "w") as f:
+                    f.write(q.content)
 
 
 if __name__ == '__main__':
